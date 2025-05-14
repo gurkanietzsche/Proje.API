a5a2cde5df256b640eb2c50fe83f5e3fa7579f84
Gürkan Yavuz <20231129018@ogr.akdeniz.edu.tr>
2025-05-14 22:37:35 +0300

    ğo

--- Proje.UI/Models/ResultDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Proje.UI.Models.DTOs
-{
-    public class ResultDTO
-    {
-        public bool Status { get; set; }
-        public string Message { get; set; }
-        public object Data { get; set; }
-    }
-}
